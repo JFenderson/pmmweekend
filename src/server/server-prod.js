b67c0e4a16e47e2549745ca4b140940e2095130b
JFenderson <fenderson.joseph@gmail.com>
2019-02-18 00:42:33 -0600

    commit

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -8,6 +8,10 @@ import config from '../../webpack.dev.config.js'
 import routes from './routes/index'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import request from 'request'
 import '@babel/polyfill'
 const app = express(),
             DIST_DIR = __dirname,
@@ -20,6 +24,8 @@ app.use(webpackDevMiddleware(compiler, {
 app.use(webpackHotMiddleware(compiler))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+app.set('trust proxy', true);
+app.set('trust proxy', 'loopback');
 app.use(cors())
 app.use(express.static(path.join(__dirname + '../../src')));
 app.use(express.static(path.join(__dirname + '../vendors')));
@@ -27,12 +33,25 @@ app.use('/api', routes);
 app.get('*', (_ ,res) => {
   res.sendFile(HTML_FILE);
 })
+app.set('trust proxy', function (ip) {
+  if (ip === '127.0.0.1' || ip === '165.227.8.2' || ip === '0.0.0.0') return true // trusted IPs
+  else return false
+})
 
-app.set('port', 3000)
-app.listen(app.get('port'), '0.0.0.0', (err) => {
-    if(err){
-      console.log(err)
-    }else{
-      console.log(`server listening on ${app.get('port')}`)
-    }
-  });
+// var ssl = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/pmmweekend.com/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/pmmweekend.com/fullchain.pem'),
+//   ca: fs.readFileSync('/etc/letsencrypt/live/pmmweekend.com/chain.pem')
+// }
+
+http.createServer(app).listen(process.env.PORT || 3000);
+// https.createServer(app).listen(process.env.PORT || 3);
+
+// app.set('port', 3000)
+// app.listen(app.get('port'), '0.0.0.0', (err) => {
+//     if(err){
+//       console.log(err)
+//     }else{
+//       console.log(`server listening on ${app.get('port')}`)
+//     }
+//   });
